c7fc7a78818ac70306430f47b5f547ef142ae020
Erick Merchant <erickmerchant@gmail.com>
2017-09-29 20:57:22 -0700

    Fix tests

--- test/src/check-dependencies.js
+++ test/src/check-dependencies.js
@@ -1,6 +1,5 @@
 const test = require('tape')
 const mockery = require('mockery')
-const path = require('path')
 
 test('test src/check-dependencies - failed require', function (t) {
   mockery.enable({
@@ -9,9 +8,19 @@ test('test src/check-dependencies - failed require', function (t) {
     warnOnUnregistered: false
   })
 
-  mockery.registerMock('david', {
-    getUpdatedDependencies: function (manifest, options, callback) {
-      callback(null, [])
+  mockery.registerMock('child_process', {
+    spawn () {
+      return {
+        stdout: {
+          on: (foo, next) => { next('{"latest": "2.1.0"}') }
+        },
+        stderr: {
+          on: () => {}
+        },
+        on (foo, next) {
+          next(0)
+        }
+      }
     }
   })
 
@@ -19,7 +28,7 @@ test('test src/check-dependencies - failed require', function (t) {
 
   t.plan(1)
 
-  checkDependencies('test', {})([]).then(function (results) {
+  checkDependencies('test/fixture/foo', {})([]).then(function (results) {
     t.deepEqual(results, [])
 
     mockery.disable()
@@ -33,48 +42,96 @@ test('test src/check-dependencies - no results', function (t) {
     warnOnUnregistered: false
   })
 
-  mockery.registerMock('david', {
-    getUpdatedDependencies: function (manifest, options, callback) {
-      callback(null, [])
+  mockery.registerMock('child_process', {
+    spawn () {
+      return {
+        stdout: {
+          on: (foo, next) => { next('{"latest": "2.1.0"}') }
+        },
+        stderr: {
+          on: () => {}
+        },
+        on (foo, next) {
+          next(0)
+        }
+      }
     }
   })
-
-  mockery.registerMock(path.join(process.cwd(), 'test', 'package.json'), {})
 
   const checkDependencies = require('../../src/check-dependencies')
 
   t.plan(1)
 
-  checkDependencies('test', {})([]).then(function (results) {
+  checkDependencies('test/fixture', {})([]).then(function (results) {
     t.deepEqual(results, [])
 
     mockery.disable()
   })
 })
 
-test('test src/check-dependencies - results', function (t) {
+test('test src/check-dependencies - upgrade', function (t) {
   mockery.enable({
     useCleanCache: true,
     warnOnReplace: false,
     warnOnUnregistered: false
   })
 
-  mockery.registerMock('david', {
-    getUpdatedDependencies: function (manifest, options, callback) {
-      callback(null, {
-        test: null
-      })
+  mockery.registerMock('child_process', {
+    spawn () {
+      return {
+        stdout: {
+          on: (foo, next) => { next('{"latest": "3.1.0"}') }
+        },
+        stderr: {
+          on: () => {}
+        },
+        on (foo, next) {
+          next(0)
+        }
+      }
     }
   })
-
-  mockery.registerMock(path.join(process.cwd(), 'test', 'package.json'), {})
 
   const checkDependencies = require('../../src/check-dependencies')
 
   t.plan(1)
 
-  checkDependencies('test', {})([]).then(function (results) {
-    t.deepEqual(results, ['upgrade test'])
+  checkDependencies('test/fixture', {})([]).then(function (results) {
+    t.deepEqual(results, ['upgrade chalk'])
+
+    mockery.disable()
+  })
+})
+
+test('test src/check-dependencies - update', function (t) {
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
+
+  mockery.registerMock('child_process', {
+    spawn () {
+      return {
+        stdout: {
+          on: (foo, next) => { next('{"latest": "2.2.0"}') }
+        },
+        stderr: {
+          on: () => {}
+        },
+        on (foo, next) {
+          next(0)
+        }
+      }
+    }
+  })
+
+  const checkDependencies = require('../../src/check-dependencies')
+
+  t.plan(1)
+
+  checkDependencies('test/fixture', {})([]).then(function (results) {
+    t.deepEqual(results, ['update chalk'])
 
     mockery.disable()
   })
