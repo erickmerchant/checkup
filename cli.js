0c9bc575ecdde860ebd7e8cc9cb352bc3549509d
Erick Merchant <erickmerchant@gmail.com>
2018-04-27 11:52:26 -0700

    Upgrade sergeant

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const command = require('sergeant')
 command('checkup', function ({parameter}) {
   parameter('directory', {
     description: 'optional directories to look at',
-    default: { value: './' }
+    default: './'
   })
 
   return action
