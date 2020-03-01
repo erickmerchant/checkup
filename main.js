80119ad0654926c173a8db094a7fb4b18517db82
Erick Merchant <erickmerchant@gmail.com>
2020-03-01 08:37:28 -0500

    Try threads

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
-const checks = require('./lib/checks.js')
-const {console} = require('./lib/globals.js')
-const {red, gray, green} = require('kleur')
-const path = require('path')
-const error = require('sergeant/error')
+const {
+  Worker, isMainThread, parentPort, workerData
+} = require('worker_threads')
 
-module.exports = (args) => {
-  const all = []
+if (isMainThread) {
+  module.exports = (args) => {
+    const all = []
 
-  for (const directory of args.directory) {
-    const name = path.relative(process.cwd(), directory) || '.'
-
-    all.push((async () => {
-      try {
-        const results = await checks(path.join(process.cwd(), directory))
-
-        if (results.length) {
-          console.log(`${red('✘')} ${name}`)
-
-          for (const result of results) {
-            console.log(`${gray(`  - ${result}`)}`)
+    for (const directory of args.directory) {
+      all.push(new Promise((resolve, reject) => {
+        const worker = new Worker(__filename, {
+          workerData: directory
+        })
+        worker.on('message', resolve)
+        worker.on('error', reject)
+        worker.on('exit', (code) => {
+          if (code !== 0) {
+            reject(new Error(`Worker stopped with exit code ${code}`))
           }
-        } else {
-          console.log(`${green('✔︎')} ${name}`)
-        }
-      } catch (err) {
+        })
+      }))
+    }
+
+    return Promise.all(all)
+  }
+} else {
+  const checks = require('./lib/checks.js')
+  const {console} = require('./lib/globals.js')
+  const {red, gray, green} = require('kleur')
+  const path = require('path')
+  const error = require('sergeant/error')
+  const directory = workerData
+  const name = path.relative(process.cwd(), directory) || '.'
+
+  ;(async () => {
+    try {
+      const results = await checks(path.join(process.cwd(), directory))
+
+      if (results.length) {
         console.log(`${red('✘')} ${name}`)
 
-        error(err)
+        for (const result of results) {
+          console.log(`${gray(`  - ${result}`)}`)
+        }
+      } else {
+        console.log(`${green('✔︎')} ${name}`)
       }
-    })())
-  }
+    } catch (err) {
+      console.log(`${red('✘')} ${name}`)
 
-  return Promise.all(all)
+      error(err)
+    }
+
+    parentPort.postMessage(true)
+  })()
 }
